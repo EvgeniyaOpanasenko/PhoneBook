18435704715f1dd0576a98821599bcc6dcc9f08b
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-09 20:54:42 +0300

    module add

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { MenuComponent } from './menu.component';
 import { AddContactComponent } from './add-contact/add-contact.component';
 import { AllContactComponent } from './all-contact/all-contact.component';
 import {CONST_ROUTING} from './app.routing';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     AddContactComponent,
-    AllContactComponent
+    AllContactComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/main/frontend/src/app/app.routing.ts
+++ frontend/src/main/frontend/src/app/app.routing.ts
@@ -1,10 +1,14 @@
 import {Routes, RouterModule} from '@angular/router';
 import {AddContactComponent} from "./add-contact/add-contact.component";
 import {AllContactComponent} from "./all-contact/all-contact.component";
+import {LoginComponent} from "./login/login.component";
+import {RegistrationComponent} from "./registration/registration.component";
 
 const MAIN_MENU_ROUTES: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'addContact', component: AddContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
   {path: 'allContact', component: AllContactComponent}
 ];
 export const CONST_ROUTING = RouterModule.forRoot(MAIN_MENU_ROUTES);
