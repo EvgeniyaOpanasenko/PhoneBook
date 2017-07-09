42e70ccf8ded250e0c2d81dbd5ebd5921558ac04
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-09 21:50:15 +0300

    table add

---
+++ frontend/src/main/frontend/src/app/contact.ts
@@ -0,0 +1,11 @@
+export class Contact {
+  constructor(private id: string,
+              private name: string,
+              private surname: string,
+              private secondName: string,
+              private cellPhone: string,
+              private homePhone: string,
+              private address: string,
+              private mail: string) {
+  }
+}
